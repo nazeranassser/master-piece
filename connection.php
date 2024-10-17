60afaa3e641334fc098ad1e2eec637050443c612
issaSroor <issasroorofficial@gmail.com>
2024-10-17 13:52:05 +0300

    add database file

--- connection.php
+++ connection.php
@@ -1,12 +1,15 @@
 <?php
-
-$server = "localhost";
+$servername = "localhost";
 $username = "root";
 $password = "";
-$db = "cake_project";
 
-$conn = new mysqli($server, $username, $password, $db);
-
-
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=cake_project", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
 ?>
 
