360840ee67315c49ae33b9c4f64234e6e43b0c66
issaSroor <issasroorofficial@gmail.com>
2024-10-20 01:08:30 +0300

    ourProductsUpdate

--- index.php
+++ index.php
@@ -266,7 +266,7 @@ $specialOccasions = $specialOccasionsQuery->fetchAll(PDO::FETCH_ASSOC);
             <div class="row">
                 <div class="col-lg-12">
                     <div class="section__text-wrap">
-                        <h1 class="section__heading u-c-secondary u-s-m-b-12">TOP TRENDING</h1>
+                        <h1 class="section__heading u-c-secondary u-s-m-b-12">OUR PRODUCTS</h1>
                         <span class="section__span u-c-silver">CHOOSE CATEGORY</span>
                     </div>
                 </div>
