632c27a764a894ba470ccdee61b3f32a40bf0ba3
shehab-A-alsanuury <shehab.alsanuury12@gmail.com>
2024-10-28 15:45:10 +0300

    done

--- libraries/Database.php
+++ libraries/Database.php
@@ -10,7 +10,7 @@ class Database {
     private $host = 'localhost';
     private $user = 'root';
     private $pass = '';
-    private $dbname = 'login-system';
+    private $dbname = 'cake_project';
 
     //Will be the PDO object
     private $dbh;
