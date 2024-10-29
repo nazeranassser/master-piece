c9bd762bdb2295853eab26b17ec7082b8a5e455d
shehab-A-alsanuury <shehab.alsanuury12@gmail.com>
2024-10-29 12:10:10 +0300

    done

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,80 +1,56 @@
-
 <footer>
-        <div class="container">
-            <div class="row">
-                  <div class="col" id="company">
-                      <img src="public/images/logo.png" alt="" class="logo">
-                      <p>
-                        We are specialized in designings, make your business a brand.
-                        Try our premium services.
-                      </p>
-                      <div class="social">
-                        <a href="#"><i class="fab fa-facebook"></i></a>
-                        <a href="#"><i class="fab fa-instagram"></i></a>
-                        <a href="#"><i class="fab fa-youtube"></i></a>
-                        <a href="#"><i class="fab fa-twitter"></i></a>
-                        <a href="#"><i class="fab fa-linkedin"></i></a>
-                      </div>
+    <div class="container">
+        <div class="row">
+              <div class="col" id="company">
+                  <img src="public/images/logo.png" alt="" class="logo">
+                  <p>
+                    Turn Every Occasion into a Celebration with Our Premium Cakes! 
+                    We are specialized in designings, make your business a brand.
+                  </p>
+                  <div class="social">
+                    <a href="#"><i class="fab fa-facebook"></i></a>
+                    <a href="#"><i class="fab fa-instagram"></i></a>
+                    <a href="#"><i class="fab fa-youtube"></i></a>
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                    <a href="#"><i class="fab fa-linkedin"></i></a>
                   </div>
+              </div>
 
+              <div class="col" id="services">
+                 <h3>Services</h3>
+                 <div class="links">
+                 <a href="#">Custom Cake Design</a>
+                 <a href="#">Cake Delivery</a>
+                 <a href="#">Online Cake Ordering</a>
+                 <a href="#">Party Consultation</a>
+                 </div>
+              </div>
 
-                  <div class="col" id="services">
-                     <h3>Services</h3>
-                     <div class="links">
-                        <a href="#">Illustration</a>
-                        <a href="#">Creatives</a>
-                        <a href="#">Poster Design</a>
-                        <a href="#">Card Design</a>
-                     </div>
+              <div class="col" id="useful-links">
+                 <h3>Links</h3>
+                 <div class="links">
+                    <a href="#">Home</a>
+                    <a href="#">About Us</a>
+                    <a href="#">Products</a>
+                    <a href="contactform.php">Contact</a>
+                 </div>
+              </div>
+
+              <div class="col" id="contact">
+                  <h3>Contact</h3>
+                  <div class="contact-details">
+                     <i class="fa fa-location"></i>
+                     <p>FF-42, Al-Jazeera Street <br> Amman, Jordan</p>
                   </div>
-
-                  <div class="col" id="useful-links">
-                     <h3>Links</h3>
-                     <div class="links">
-                        <a href="#">About</a>
-                        <a href="#">Services</a>
-                        <a href="#">Our Policy</a>
-                        <a href="#">Help</a>
-                     </div>
+                  <div class="contact-details">
+                     <i class="fa fa-phone"></i>
+                     <p>+1-8755856858</p>
                   </div>
+              </div>
+        </div>
+    </div>
+</footer>
 
-                  <div class="col" id="contact">
-                      <h3>Contact</h3>
-                      <div class="contact-details">
-                         <i class="fa fa-location"></i>
-                         <p>FF-42, Procube Avenue, <br> NY, USA</p>
-                      </div>
-                      <div class="contact-details">
-                         <i class="fa fa-phone"></i>
-                         <p>+1-8755856858</p>
-                      </div>
-                  </div>
-            </div>
-
-            <!-- <div class="preloader is-active">
-        <div class="preloader__wrap">
-
-            <img class="preloader__img" src="images/preloader.png" alt=""></div>
-    </div> -->
-
-            <!-- <div class="row">
-                  <div class="form">
-                    <form action="">
-                        <input type="text" placeholder="Email here...">
-                        <button><i class="fa fa-paper-plane"></i></button>
-                    </form>
-                  </div>
-            </div>
-
-        </div> -->
-     </footer>
-
-     <!-- <script>
-    window.addEventListener('load', function() {
-        document.querySelector('.preloader').classList.remove('is-active');
-    });
-</script> -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-
 </body>
 </html>
